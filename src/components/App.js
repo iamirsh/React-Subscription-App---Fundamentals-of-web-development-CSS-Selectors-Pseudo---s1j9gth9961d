bf752f937890bc82fd671bfc3e4b2327d9037771
Irshad Ahmad <luckyirsh786@gmail.com>
2022-11-17 16:21:46 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,36 @@
 import React from "react";
 import "../styles/App.css";
+
+
 const App = () => {
   return (
     <div id="main">
-      <div className="container">
-
-        <div className="community">
-            <h2 className="community-header">Join our community</h2>
-            <p className="days">30-day, hassle-free monkey back gaurantee</p>
-            <p>Gain access to our full library of tutorials along with expert code reviews. Perfect for any
-                developers who are serious about honing their skills</p>
+      <div class="container">
+        <div class="community">
+          <h2 class="community-header">Join our community</h2>
+          <p class="community-title-meta">30-day, hassle-free monkey back gurarantee</p>
+          <p class="community-desciption">Gain access to our full library of tutorials along with experts code reviews. Perfect for any developers who are serious about honing thier skills.
+          </p>
         </div>
-
-        <div className="card-details">
-            <div className="card">
-                <p className="card-header">Monthly Subscription</p>
-                <p><span id="doller">$29</span> Per Month</p>
-                <p>Full access for less than $1 a day.</p>
-                <button id="btn">Sign Up</button>
-            </div>
-
-            <div className="why-us">
-                <h4 className="why-us-header">Why Us</h4>
-                <p>Tutorial by industry experts</p>
-                <p>Peer & expert code review </p>
-                <p>Coding exercises</p>
-                <p>Access you our Github repos</p>
-                <p>Community forum</p>
-                <p>Flashcard decks</p>
-            </div>
+        <div class="card-details">
+          <div class="card">
+            <p class="card-header">Monthly Subscription</p>
+            <span><strong class="price">$29</strong> per month</span>
+            <span>Full access for less than $1 a day.</span>
+            <button type="button" class="sign-up-btn">Sign Up</button>
+          </div>
+          <div class="why-us">
+            <h4 class="why-us-header">Why Us</h4>
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer & expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access yo our GitHub repos</li>
+              <li>Community forum</li>
+              <li>Flashcard decks</li>
+            </ul>
+          </div>
         </div>
-
-
       </div>
     </div>
   );
